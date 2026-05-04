867513737476eec630ba8635e677cb6ce5c71aa1
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-05-03 22:31:09 -1200

    Fix syntax error in model loading line

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import joblib
 
- model = joblib.load("/content/fraud_detection_pipelie.pkl"
+ model = joblib.load("/content/fraud_detection_pipelie.pkl")
 
 st.title('🎈Fraud detection prediction app' )
 
