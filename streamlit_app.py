57d5a9ab7fbb2c928f080a92d76f15f688970e97
osbornjunior015-sys <osbornjunior015@gmail.com>
2026-05-03 22:36:40 -1200

    Fix model loading path in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np 
 import joblib
 
-model = joblib.load("/content/fraud_detection_pipelie.pkl")
+model = joblib.load("/content/fraud_detection_app.py")
 
 st.title('🤖 Fraud detection prediction app')
 
